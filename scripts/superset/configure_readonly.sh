80a49376f7e5836a41dec380677f9a29f86351f4
Munzer <munzermaraqa@gmail.com>
2026-05-21 18:54:14 +0100

    Render Superset readonly SQL safely

--- scripts/superset/configure_readonly.sh
+++ scripts/superset/configure_readonly.sh
@@ -9,19 +9,32 @@ source "$ROOT_DIR/install/helpers.sh"
 require_cmd kubectl
 
 postgres_password_quoted="$(printf '%q' "$POSTGRES_PASSWORD")"
-superset_readonly_user_quoted="$(printf '%q' "$SUPERSET_READONLY_USER")"
-superset_readonly_password_quoted="$(printf '%q' "$SUPERSET_READONLY_PASSWORD")"
 postgres_user_quoted="$(printf '%q' "$POSTGRES_USER")"
 postgres_db_quoted="$(printf '%q' "$POSTGRES_DB")"
+
+sql_literal() {
+  printf "'%s'" "$(printf '%s' "$1" | sed "s/'/''/g")"
+}
+
+sql_identifier() {
+  printf '"%s"' "$(printf '%s' "$1" | sed 's/"/""/g')"
+}
+
+rendered_sql="$(mktemp)"
+sed \
+  -e "s/__SUPERSET_READONLY_USER_LITERAL__/$(sql_literal "$SUPERSET_READONLY_USER")/g" \
+  -e "s/__SUPERSET_READONLY_PASSWORD_LITERAL__/$(sql_literal "$SUPERSET_READONLY_PASSWORD")/g" \
+  -e "s/__SUPERSET_READONLY_USER_IDENTIFIER__/$(sql_identifier "$SUPERSET_READONLY_USER")/g" \
+  "$ROOT_DIR/sql/superset/superset_readonly.sql" > "$rendered_sql"
 
 log "Applying Superset read-only role grants"
 kubectl -n "$NAMESPACE" exec -i statefulset/postgres -- bash -lc \
   "PGPASSWORD=${postgres_password_quoted} psql \
     -v ON_ERROR_STOP=1 \
-    -v SUPERSET_READONLY_USER=${superset_readonly_user_quoted} \
-    -v SUPERSET_READONLY_PASSWORD=${superset_readonly_password_quoted} \
     -U ${postgres_user_quoted} \
     -d ${postgres_db_quoted} \
-    -f -" < "$ROOT_DIR/sql/superset/superset_readonly.sql"
+    -f -" < "$rendered_sql"
+
+rm -f "$rendered_sql"
 
 log_success "Superset read-only role configured"
